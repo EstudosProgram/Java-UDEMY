f90872f6aa689fd08d2fcc2d1b79ce0dfa0a89cc
Milena <milena.cod@gmail.com>
2025-02-19 19:35:45 -0300

    update

---
+++ src/Sessao4/Casting.java
@@ -0,0 +1,19 @@
+package Sessao4;
+
+public class Casting {
+    public static void main(String[] args) {
+
+        int a, b;
+        double resultado;
+
+        a = 2;
+        b = 5;
+
+        resultado = (double) (a + b)/2;
+        System.out.println(resultado);
+    }
+
+
+
+
+}
