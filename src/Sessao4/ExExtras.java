c2f54eb29bc8650e8f6f3e5ea2777ae6612b7c60
Milena <milena.cod@gmail.com>
2025-03-13 19:15:01 -0300

    resolvendo exercicios

--- src/Sessao4/ExExtras.java
+++ src/Sessao4/ExExtras.java
@@ -64,10 +64,49 @@ public class ExExtras {
         double salario = horasWorked * rendaPorHora;
 
         System.out.printf("NUMBER: %d %n", numFuncionario);
-        System.out.printf("SALARY: U$ %.2f", salario);
+        System.out.printf("SALARY: U$ %.2f%n", salario);
 
         //Ex 5
 
+        int numPeca, numPeca2;
+        int qntPeca, qntPeca2;
+        double precoUni, precoUni2;
+
+        System.out.println("Digite o código da peça, a quantidade e o valor unitário");
+        numPeca = sc.nextInt();
+        qntPeca = sc.nextInt();
+        precoUni = sc.nextDouble();
+
+        System.out.println("Digite o código da peça, a quantidade e o valor unitário do segundo produto");
+
+        numPeca2 = sc.nextInt();
+        qntPeca2 = sc.nextInt();
+        precoUni2 = sc.nextDouble();
+
+        double precoAPagar = ((qntPeca * precoUni) + (qntPeca2 * precoUni2));
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f%n", precoAPagar);
+
         //Ex 6
+
+        double a1, b1, c1;
+        double pi = 3.14159;
+
+        System.out.println("Digite o número de a, b e c:");
+        a1 = sc.nextFloat();
+        b1 = sc.nextFloat();
+        c1 = sc.nextFloat();
+
+        double triangulo = (a1 * c1) / 2;
+        double circulo = Math.pow(c1, 2) * pi;
+        double trapezio = ((a1 + b1) * c1) / 2;
+        double quadrado = b1 * b1;
+        double retangulo = a1 * b1;
+
+        System.out.printf("TRIANGULO: %.3f%n", triangulo);
+        System.out.printf("CIRCULO: %.3f%n", circulo);
+        System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+        System.out.printf("QUADRADO: %.3f%n", quadrado);
+        System.out.printf("RETANGULO: %.3f", retangulo);
     }
 }
